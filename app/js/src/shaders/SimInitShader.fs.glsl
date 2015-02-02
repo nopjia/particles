c805eae4c14a012a360d1d41224bace86de1c370
Nop Jiarathanakul <nop@iamnop.com>
2015-02-01 22:59:13 -0800

    init to sphere, with continuous colors along y

--- app/js/src/shaders/SimInitShader.fs.glsl
+++ app/js/src/shaders/SimInitShader.fs.glsl
@@ -1,3 +1,7 @@
+#define M_PI    3.14159265358979323846264338327950
+#define M_2PI   6.28318530717958647692528676655900
+#define M_PI2   1.57079632679489661923132169163975
+
 varying vec2 vUv;
 
 uniform sampler2D tDiffuse;
@@ -10,12 +14,23 @@ float rand(vec2 seed) {
 
 void main() {
     // vec3 pos = vec3(vUv.x, vUv.y, rand(vUv));
-    vec3 pos = vec3(vUv.x, vUv.y, 0.0);
 
-    // transform
-    vec3 center = vec3(0.0, 0.0, 0.0);
-    vec3 size = vec3(1.0, 1.0, 1.0);
-    pos = pos*size + center - size/2.0;
+    // square sheet
+    // vec3 pos = vec3(vUv.x, vUv.y, 0.0);
+    // vec3 center = vec3(0.0, 0.0, 0.0);
+    // vec3 size = vec3(1.0, 1.0, 1.0);
+    // pos = pos*size + center - size/2.0;
+
+    // sphere, continuous along vUv.y
+    vec2 coords = vUv;
+    coords.x = coords.x * M_2PI - M_PI; // theta (lat)
+    coords.y = coords.y * M_PI;         // phi (long)
+    vec3 sphereCoords = vec3(
+        sin(coords.y) * cos(coords.x),
+        cos(coords.y),
+        sin(coords.y) * sin(coords.x)
+    );
+    vec3 pos = sphereCoords;
 
     gl_FragColor = vec4(vec3(pos), 1.0);
 }
