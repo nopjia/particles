e4f188b36fd857a414fefb3e09934395b312d160
Nop Jiarathanakul <prutsdomj@gmail.com>
2015-02-02 20:38:56 -0800

    use orbit camera and auto rotate

--- app/js/src/shaders/ParticleShader.vs.glsl
+++ app/js/src/shaders/ParticleShader.vs.glsl
@@ -1,3 +1,6 @@
+#define PS_CAM_MAX_DIST 12.0
+#define PS_MAX_SIZE 20.0
+
 uniform sampler2D tPos;
 
 varying vec3 vPos;
@@ -9,8 +12,10 @@ void main() {
     vec3 pos = posSample.rgb;
 
     vec3 camToPos = pos - cameraPosition;
+    float camDist = length(camToPos);
 
-    gl_PointSize = max(45.0 / length(camToPos), 1.0);
+    // gl_PointSize = max(PS_MAX_SIZE*(1.0-camDist/PS_CAM_MAX_DIST), 1.0);
+    gl_PointSize = max(3.0*PS_CAM_MAX_DIST/camDist, 1.0);
 
     gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
 }
