7cd76a7268a94ff77ef26003911b8a2fd6b7286d
Nop Jiarathanakul <nop@iamnop.com>
2015-03-01 23:02:42 -0800

    UVMapper class

---
+++ app/js/src/UVMapper.js
@@ -0,0 +1,29 @@
+var UVMapper = function(renderer) {
+    var _renderer = renderer;
+
+    var _camera = new THREE.OrthographicCamera( -1, 1, 1, -1, 0, 1 );   // not really used
+    var _scene  = new THREE.Scene();
+    _scene.overrideMaterial = createShaderMaterial(UVMapShader);
+
+    this.renderMap = function(mesh, target) {
+        // might need to make geo, so don't steal from its original parent
+        _scene.add(mesh);
+        _renderer.render(_scene, _camera, target);
+        _scene.remove(mesh);
+    };
+
+    this.getMap = function(mesh, size) {
+        var target = new THREE.WebGLRenderTarget(size, size, {
+            minFilter: THREE.LinearFilter,
+            magFilter: THREE.LinearFilter,
+            format: THREE.RGBAFormat,
+            depthBuffer: false,
+            stencilBuffer: false
+        });
+        target.generateMipmaps = false;
+
+        this.renderMap(mesh, target);
+
+        return target;
+    };
+};
