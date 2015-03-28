96752fdb5fe8e8bd2c92653c9d6aa77fd6fad3d6
Nop Jiarathanakul <nop@iamnop.com>
2015-03-27 23:04:40 -0700

    uvtest.html accepts parameters

--- app/js/src/Utils.js
+++ app/js/src/Utils.js
@@ -117,3 +117,10 @@ Utils.toggleFullscreen = function() {
         }
     }
 };
+
+Utils.getParameterByName = function(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+};
