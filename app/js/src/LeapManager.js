0c81158a2e81724bea6edbcefa73e1f120607932
Nop Jiarathanakul <nop@iamnop.com>
2015-03-30 01:50:44 -0700

    leap manager can track all joint positions

--- app/js/src/LeapManager.js
+++ app/js/src/LeapManager.js
@@ -1,35 +1,53 @@
-var LeapManager = function(renderer, camera) {
+var LeapManager = function(renderer, camera, transform) {
     var _renderer = renderer;
     var _camera = camera;
+    var _tmat = transform;
 
-    var _LEAP_SCALE = new THREE.Vector3(0.01,0.01,0.01);
-    var _LEAP_POSITION = new THREE.Vector3(0.0, -2.0, -5.0);
 
-    var _tmat = new THREE.Matrix4();
-    _tmat.scale(_LEAP_SCALE);
-    _tmat.setPosition(_LEAP_POSITION);
+    // PUBLIC
 
-    var _scene = new THREE.Scene();
-    _scene.overrideMaterial = new THREE.MeshBasicMaterial({
-        color: 0xffffff,
-        opacity: 0.25,
-    });
+    this.frame = undefined;
+    this.palmPositions = [new THREE.Vector3(), new THREE.Vector3()];
+    this.jointPositions = []; for (var i=0; i<50; i++) this.jointPositions.push(new THREE.Vector3());
+    this.activeJointCount = 0;
 
-    var _root = new THREE.Object3D();
-    _root.matrixAutoUpdate = false;
-    _scene.add(_root);
-
-    var _leapController = new Leap.Controller();
-    _leapController.use('boneHand', {
-        render: function() {},
-        scene: _root,
-        arm: false,
-    }).connect();
+    // FUNCTIONS
 
     this.update = function() {
         // update leapRoot transform
         _root.matrix.multiplyMatrices(_camera.matrix, _tmat);
         _root.matrixWorldNeedsUpdate = true;
+
+        this.frame = _controller.frame();
+
+        // TODO_NOP: dangerous much? reset things to undefined
+        this.palmPositions[0].set();
+        this.palmPositions[1].set();
+        for (var i=0; i<50; i++)
+            this.jointPositions[i].set();
+
+        // extract palm positions
+        for (var h=0; h<this.frame.hands.length; h++) {
+            var rawpos = this.frame.hands[h].palmPosition;
+            this.palmPositions[h].set(rawpos[0],rawpos[1],rawpos[2]);
+            this.palmPositions[h].applyMatrix4(_root.matrix);
+        }
+
+        // extract joint positions
+        this.activeJointCount = this.frame.hands.length * 25;
+        var jointIdx = 0;
+        for (var h=0; h<this.frame.hands.length; h++) {
+            var hand = this.frame.hands[h];
+            for (var f=0; f<hand.fingers.length; f++) {
+                var finger = hand.fingers[f];
+                for (var j=0; j<finger.positions.length; j++) {
+                    var rawpos = finger.positions[j];
+                    this.jointPositions[jointIdx].set(rawpos[0],rawpos[1],rawpos[2]);
+                    this.jointPositions[jointIdx].applyMatrix4(_root.matrix);
+                    jointIdx++;
+                }
+            }
+        }
     };
 
     this.render = function(renderTarget, forceClear) {
@@ -47,4 +65,24 @@ var LeapManager = function(renderer, camera) {
     this.setTransform = function(mat) {
         _tmat = mat;
     };
+
+
+    // INIT
+
+    var _scene = new THREE.Scene();
+    _scene.overrideMaterial = new THREE.MeshBasicMaterial({
+        color: 0xffffff,
+        opacity: 0.25,
+    });
+
+    var _root = new THREE.Object3D();
+    _root.matrixAutoUpdate = false;
+    _scene.add(_root);
+
+    var _controller = new Leap.Controller();
+    _controller.use('boneHand', {
+        render: function() {},
+        scene: _root,
+        arm: false,
+    }).connect();
 };
