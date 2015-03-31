7823a038980816ab759ecf328b4f56dcdf88b2c5
Nop Jiarathanakul <nop@iamnop.com>
2015-03-31 02:07:34 -0700

    refactored LeapManager made some vars public commented out joint positions

--- app/js/src/LeapManager.js
+++ app/js/src/LeapManager.js
@@ -1,7 +1,9 @@
 var LeapManager = function(renderer, camera, transform) {
-    var _renderer = renderer;
-    var _camera = camera;
-    var _tmat = transform;
+    var _scene, _root, _controller;
+
+    this.renderer = renderer;
+    this.camera = camera;
+    this.matrix = transform;
 
 
     // PUBLIC
@@ -11,84 +13,68 @@ var LeapManager = function(renderer, camera, transform) {
     // read-only
     this.frame = undefined;
     this.palmPositions = [new THREE.Vector3(), new THREE.Vector3()];
-    this.jointPositions = []; for (var i=0; i<50; i++) this.jointPositions.push(new THREE.Vector3());
     this.activeHandCount = 0;
-    this.activeJointCount = 0;
+    // this.jointPositions = []; for (var i=0; i<50; i++) this.jointPositions.push(new THREE.Vector3());
+    // this.activeJointCount = 0;
+
 
     // FUNCTIONS
 
     this.update = function() {
         // update leapRoot transform
         if (this.followCamera)
-            _root.matrix.multiplyMatrices(_camera.matrix, _tmat);
+            _root.matrix.multiplyMatrices(this.camera.matrix, this.matrix);
         else
-            _root.matrix.copy(_tmat);
+            _root.matrix.copy(this.matrix);
         _root.matrixWorldNeedsUpdate = true;
 
+        // grab frame data
         this.frame = _controller.frame();
 
-        // TODO_NOP: dangerous much? reset things to undefined
-        this.palmPositions[0].set();
-        this.palmPositions[1].set();
-        for (var i=0; i<50; i++)
-            this.jointPositions[i].set();
-
-        this.activeHandCount = this.frame.hands.length;
-        this.activeJointCount = this.activeHandCount * 25;
-
         // extract palm positions
+        this.activeHandCount = this.frame.hands.length;
         for (var h=0; h<this.frame.hands.length; h++) {
-            var rawpos = this.frame.hands[h].palmPosition;
-            this.palmPositions[h].set(rawpos[0],rawpos[1],rawpos[2]);
+            var pos = this.frame.hands[h].palmPosition;
+            this.palmPositions[h].set(pos[0],pos[1],pos[2]);
             this.palmPositions[h].applyMatrix4(_root.matrix);
         }
 
+        // this.activeJointCount = this.activeHandCount * 25;
+
         // extract joint positions
-        var jointIdx = 0;
-        for (var h=0; h<this.frame.hands.length; h++) {
-            var hand = this.frame.hands[h];
-            for (var f=0; f<hand.fingers.length; f++) {
-                var finger = hand.fingers[f];
-                for (var j=0; j<finger.positions.length; j++) {
-                    var rawpos = finger.positions[j];
-                    this.jointPositions[jointIdx].set(rawpos[0],rawpos[1],rawpos[2]);
-                    this.jointPositions[jointIdx].applyMatrix4(_root.matrix);
-                    jointIdx++;
-                }
-            }
-        }
+        // var jointIdx = 0;
+        // for (var h=0; h<this.frame.hands.length; h++) {
+        //     var hand = this.frame.hands[h];
+        //     for (var f=0; f<hand.fingers.length; f++) {
+        //         var finger = hand.fingers[f];
+        //         for (var j=0; j<finger.positions.length; j++) {
+        //             var rawpos = finger.positions[j];
+        //             this.jointPositions[jointIdx].set(rawpos[0],rawpos[1],rawpos[2]);
+        //             this.jointPositions[jointIdx].applyMatrix4(_root.matrix);
+        //             jointIdx++;
+        //         }
+        //     }
+        // }
     };
 
     this.render = function(renderTarget, forceClear) {
-        _renderer.render(_scene, _camera, renderTarget, forceClear);
-    };
-
-    this.setCamera = function(camera) {
-        _camera = camera;
-    };
-
-    this.setRenderer = function(renderer) {
-        _renderer = renderer;
-    };
-
-    this.setTransform = function(mat) {
-        _tmat = mat;
+        this.renderer.render(_scene, this.camera, renderTarget, forceClear);
     };
 
 
     // INIT
 
-    var _scene = new THREE.Scene();
+    _scene = new THREE.Scene();
     _scene.overrideMaterial = new THREE.MeshBasicMaterial({
         color: 0xffffff,
         opacity: 0.25,
     });
 
-    var _root = new THREE.Object3D();
+    _root = new THREE.Object3D();
     _root.matrixAutoUpdate = false;
     _scene.add(_root);
 
-    var _controller = new Leap.Controller();
+    _controller = new Leap.Controller();
     _controller.use('boneHand', {
         render: function() {},
         scene: _root,
